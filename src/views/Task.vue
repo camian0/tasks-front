cdf15709d4328545b52459060d16a40d266e402b
camian <camian6@outlook.com>
2025-02-06 12:26:07 -0500

    main view for tasks was added

---
+++ src/views/Task.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>Bienvenido a la vista de tareas</p>
+
+  {{ this.apiUrl }}
+</template>
+<script>
+export default {
+  name: "TaskVue",
+  data() {
+    return {
+      apiUrl: import.meta.env.VITE_API_ENDPOINT_URL,
+    };
+  },
+};
+</script>
+<style scope></style>
