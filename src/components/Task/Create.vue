ec953eb58a4b14258e19c8a9cf95857e3a81f7a3
camian <camian6@outlook.com>
2025-02-07 14:53:40 -0500

    Component to create a task was created

---
+++ src/components/Task/Create.vue
@@ -0,0 +1,25 @@
+<template>
+  <p>Bienvenido a Crear una tarea component</p>
+</template>
+
+<script>
+export default {
+  name: "CreateTask",
+  data() {
+    return {
+      task_name: "",
+      email: "",
+      finish_date: "",
+      task_state: "",
+      file_name: "",
+    };
+  },
+  methods: {
+    async createTask() {
+      console.log("Creando tarea");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
