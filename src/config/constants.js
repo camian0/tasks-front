41d0f34a7a26abd21750fba04c32f8f9c6442f97
camian <camian6@outlook.com>
2025-02-08 00:09:27 -0500

    function to convert xml response to json was added

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,12 @@
+import { XMLParser } from "fast-xml-parser";
+
 export const APP_HOST = import.meta.env.VITE_API_ENDPOINT_URL;
+
+
+
+export async function convertXML(url) {
+    const parser = new XMLParser();
+    const jsonResult = parser.parse(text);
+
+    console.log(jsonResult);
+}
