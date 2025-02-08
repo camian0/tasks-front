2e147cf599d8550b07607ba9a3fc746b5862c24e
camian <camian6@outlook.com>
2025-02-08 18:31:36 -0500

    component to delete the selected task was finished

--- src/components/Task/Delete.vue
+++ src/components/Task/Delete.vue
@@ -5,12 +5,39 @@
 </template>
 
 <script>
+import { deleteData } from "../../request/request";
 export default {
   name: "DeleteTask",
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+  },
+  watch: {
+    id(newVal, oldVal) {
+      console.log(`ID changed from ${oldVal} to ${newVal}`);
+      this.deleteTask();
+    },
+  },
   data() {
     return {};
   },
-  methods: {},
+  methods: {
+    async deleteTask() {
+      console.log("Eliminando tarea con id", this.id);
+      if (!confirm("Are you sure you want to delete this task?")) {
+        console.log("Delete canceled");
+        return;
+      }
+      let response = await deleteData(`tasks/${this.id}`);
+      console.log("response", response);
+      //   TODO event to update list
+      if (response.status === 200) {
+        console.log(response.data.message);
+      }
+    },
+  },
 };
 </script>
 
