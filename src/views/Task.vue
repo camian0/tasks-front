315bacde988c4f8c13c73b30ef1ebc997be6e2c6
camian <camian6@outlook.com>
2025-02-08 00:11:41 -0500

    create task componenw as added to view

--- src/views/Task.vue
+++ src/views/Task.vue
@@ -1,14 +1,17 @@
 <template>
   <p>Bienvenido a la vista de tareas</p>
   <ListTask />
+  <CreateTask />
 </template>
 <script>
 import ListTask from "../components/Task/List.vue";
+import CreateTask from "../components/Task/Create.vue";
 
 export default {
   name: "TaskView",
   components: {
     ListTask,
+    CreateTask,
   },
   data() {
     return {};
