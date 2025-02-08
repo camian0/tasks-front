ff3512e89f967221a52a2081ff0c7814ee8bea82
camian <camian6@outlook.com>
2025-02-08 17:19:38 -0500

    delete task component was added

---
+++ src/components/Task/Delete.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Bienvenido al componente eliminar tarea</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DeleteTask",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped></style>
