44b358bec92fdf646c7e269d6e110fd69714bbb8
camian <camian6@outlook.com>
2025-02-08 20:02:44 -0500

    the way to mount app was changed for add icons

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import './assets/main.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
+app.mount('#app')
