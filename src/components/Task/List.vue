05b2e82afe9772cd251b81f782cadc477535dd2d
camian <camian6@outlook.com>
2025-02-08 20:03:23 -0500

    The element plus table was added on list component
    the table, notifications and loaders were added to list component

--- src/components/Task/List.vue
+++ src/components/Task/List.vue
@@ -1,7 +1,37 @@
 <template>
   <div>
     <p>Listar tareas</p>
-    <table>
+    <el-table
+      v-loading="loadingTable"
+      :data="listTask"
+      empty-text="No hay tareas para mostrar"
+      stripe
+      style="width: 100%"
+    >
+      <el-table-column prop="id" label="id" :min-width="20" />
+      <el-table-column prop="task_name" label="Nombre de la tarea" :min-width="100" />
+      <el-table-column prop="email" label="Correo" :min-width="120" />
+      <el-table-column
+        label="Fecha finalizacion"
+        prop="finish_date"
+        :formatter="formatDate"
+        :min-width="95"
+      />
+      <!-- <el-table-column prop="task_state" label="Estado de la tarea" /> -->
+      <el-table-column
+        label="Estado de la tarea"
+        prop="task_state"
+        :formatter="formatStatusStask"
+        :min-width="95"
+      />
+      <el-table-column prop="file_name" label="Nombre del archivo" />
+      <el-table-column label="Acciones" align="center" width="160">
+        <template #default="scope">
+          <el-icon><Plus /></el-icon>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- <table>
       <thead>
         <tr>
           <th>Id</th>
@@ -28,20 +58,31 @@
           </td>
         </tr>
       </tbody>
-    </table>
-    <p v-if="this.listTask.length == 0">No hay tareas para mostrar</p>
+    </table> -->
   </div>
 </template>
 
 <script>
 import { getData } from "../../request/request";
-
+import { ref } from "vue";
 export default {
   name: "ListTask",
-  emits: ["oneTask", "updateTask", "deleteTask"], // Declara el evento aquí
+  emits: ["oneTask", "updateTask", "deleteTask"],
+  setup() {
+    const loadingTable = ref(false);
+
+    return {
+      loadingTable,
+    };
+  },
   data() {
     return {
       listTask: [],
+      taskStatus: {
+        "TO DO": "PENDIENTE",
+        PROCESS: "EN PROCESO",
+        FINISHED: "FINALIZADO",
+      },
     };
   },
   mounted() {
@@ -49,18 +90,30 @@ export default {
   },
   methods: {
     async getTask() {
+      this.loadingTable = true;
       let response = await getData("tasks");
       if (response.status === 200) {
-        console.log(response.data.message);
         this.listTask = response.data.items;
+        this.loadingTable = false;
+        ElNotification({
+          title: "Exito",
+          message: response.data.message,
+          type: "success",
+        });
         return;
       }
-      // TODO Sacar las notificaciones bonitas
-      console.log(response.data.message);
+      ElNotification({
+        title: "Error",
+        message: response.data.message,
+        type: "error",
+      });
     },
 
-    formatDate(receivedDate) {
-      const date = new Date(receivedDate);
+    formatDate(row, column, cellValue) {
+      if (!cellValue) {
+        return "";
+      }
+      const date = new Date(cellValue);
       const year = date.getUTCFullYear();
       const month = String(date.getUTCMonth() + 1).padStart(2, "0"); // month comienza desde 0
       const day = String(date.getUTCDate()).padStart(2, "0");
@@ -68,6 +121,11 @@ export default {
       const minutes = String(date.getUTCMinutes()).padStart(2, "0");
 
       return `${day}/${month}/${year} ${hour}:${minutes}`;
+    },
+
+    formatStatusStask(row, column, cellValue) {
+      cellValue = cellValue.toUpperCase();
+      return this.taskStatus[cellValue];
     },
 
     sendOneTask(event) {
@@ -79,7 +137,6 @@ export default {
     },
 
     handleDeleteTask(id) {
-      console.log("Tarea para eliminar recibida con id", id);
       this.$emit("deleteTask", id);
     },
   },
