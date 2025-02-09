b9512a959223109284bdec48b82dc1702e17c877
camian <camian6@outlook.com>
2025-02-09 15:11:51 -0500

    delete component finished
    the component to delete task was finished, also the main app was
    modified to add style to use a component to element plus

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import './assets/main.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -11,5 +13,5 @@ const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
-
+app.use(ElementPlus);
 app.mount('#app')
